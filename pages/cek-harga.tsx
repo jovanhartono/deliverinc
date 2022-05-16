c9e9a3f0cf57656fc114c3bb40da33dcec34faaf
jovan <jovanus@yahoo.co.id>
2022-05-16 13:13:10 +0700

    adding price checking page

---
+++ pages/cek-harga.tsx
@@ -0,0 +1,19 @@
+import {NextPage} from "next";
+import Head from "next/head";
+
+const CekHarga: NextPage = () => {
+  return (
+      <>
+          <Head>
+              <title>Cek Harga - Deliverinc</title>
+              <meta name="description"
+                    content="Cek Harga - Deliverinc Importir dan Forwarder China Indonesia Terpercaya"/>
+          </Head>
+        <main>
+          <h1>Cek Harga</h1>
+        </main>
+      </>
+  );
+};
+
+export default CekHarga;
